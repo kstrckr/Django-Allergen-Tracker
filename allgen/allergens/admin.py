c703dd1439ac6c040f852b29c340dedd27657669
Kurt Strecker <ksphotopro@gmail.com>
2017-10-26 19:54:05 -0400

    added Peanuts and a Peanuts Comment

--- allgen/allergens/admin.py
+++ allgen/allergens/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Allergen, Comment
 # Register your models here.
+
+admin.site.register(Allergen)
+admin.site.register(Comment)
--- allgen/allergens/models.py
+++ allgen/allergens/models.py
@@ -7,10 +7,17 @@ from django.db import models
 # Create your models here.
 
 class Allergen(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
     servings = models.IntegerField(default=0)
-    
+
+    def __str__(self):
+        return self.name
+
 class Comment(models.Model):
     comment_text = models.CharField(max_length=255)
     reaction = models.BooleanField(default=False)
     allergen = models.ForeignKey(Allergen, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(timezone.now())
+
+    def __str__(self):
+        return '{} Comment posted on {}'.format(self.allergen, self.created_on)
